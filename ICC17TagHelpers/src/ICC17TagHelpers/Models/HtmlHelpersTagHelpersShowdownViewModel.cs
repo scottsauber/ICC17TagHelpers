b1f993a987c20eacdd8fd5ebcda448334d71aec6
Sauber, Scott <SSauber@iowabankers.com>
2016-07-20 21:29:06 -0500

    Compare HTML Helpers and Tag Helpers code

---
+++ ICC17TagHelpers/src/ICC17TagHelpers/Models/HtmlHelpersTagHelpersShowdownViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICC17TagHelpers.Models
+{
+    public class HtmlHelpersTagHelpersShowdownViewModel
+    {
+        public string Name { get; set; }
+    }
+}
