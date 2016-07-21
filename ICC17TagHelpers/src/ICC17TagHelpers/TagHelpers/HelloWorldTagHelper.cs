d7eb349c4454557caabb581a6b599fc7886b198b
Sauber, Scott <SSauber@iowabankers.com>
2016-07-20 21:09:53 -0500

    Initial Commit.  Hello World Tag Helper

---
+++ ICC17TagHelpers/src/ICC17TagHelpers/TagHelpers/HelloWorldTagHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace ICC17TagHelpers.TagHelpers
+{
+    public class HelloWorldTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetContent("Hello Iowa Code Camp");
+        }
+    }
+}
