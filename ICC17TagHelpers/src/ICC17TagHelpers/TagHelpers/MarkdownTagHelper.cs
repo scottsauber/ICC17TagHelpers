fd5574fe97a2e77ea27fb957f90da4c85e08eb65
Sauber, Scott <SSauber@iowabankers.com>
2016-07-20 21:43:41 -0500

    Markdown tag helper

---
+++ ICC17TagHelpers/src/ICC17TagHelpers/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICC17TagHelpers.TagHelpers
+{
+    public class MarkdownTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var childContent = await output.GetChildContentAsync();
+            string rawContent = childContent.GetContent();
+            var markdown = Markdown.ToHtml(rawContent)
+            output.Content.SetHtmlContent(markdown);
+        }
+    }
+}
