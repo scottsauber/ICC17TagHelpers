449d2a095771e5db1a109b1ee70b96a7cfde1a3f
Sauber, Scott <SSauber@iowabankers.com>
2016-07-23 01:39:59 -0500

    Add MailTo TagHelper Add Powerpoint

---
+++ ICC17TagHelpers/src/ICC17TagHelpers/TagHelpers/MailToTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICC17TagHelpers.TagHelpers
+{
+    public class MailToTagHelper : TagHelper
+    {
+        public string Email { get; set; }
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var content = output.GetChildContentAsync();
+            output.Attributes.Add("href", $"mailto:{Email}");
+
+
+            output.TagName = "a";
+        }
+    }
+}
